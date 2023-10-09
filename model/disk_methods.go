0c16aebaf1c01afdc218b60a608f73a5df59a952
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-09 19:04:06 +0200

    Create disk_methods.go

---
+++ model/disk_methods.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"path/filepath"
+
+	"github.com/wissemmansouri/OpenIT.one-Common/utils/file"
+)
+
+const defaultMountPath = "/media"
+
+func (m *LSBLKModel) GetMountPoint(name string) string {
+	if name == "" {
+		name = "Storage_" + m.Name
+	}
+
+	if m.Label != "" {
+		name += "_" + m.Label
+	}
+
+	if m.Model != "" {
+		name += "_" + m.Model
+	}
+	mountPoint := filepath.Join(defaultMountPath, name)
+	if file.CheckNotExist(mountPoint) {
+		return mountPoint
+	}
+	return mountPoint + "_" + m.Name
+}
