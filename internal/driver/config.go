a5d29564a8fde10952bf8dc4b5ad45b7c337607e
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-17 18:48:33 +0200

    Create config.go

---
+++ internal/driver/config.go
@@ -0,0 +1,17 @@
+package driver
+
+type Config struct {
+	Name        string `json:"name"`
+	LocalSort   bool   `json:"local_sort"`
+	OnlyLocal   bool   `json:"only_local"`
+	OnlyProxy   bool   `json:"only_proxy"`
+	NoCache     bool   `json:"no_cache"`
+	NoUpload    bool   `json:"no_upload"`
+	NeedMs      bool   `json:"need_ms"` // if need get message from user, such as validate code
+	DefaultRoot string `json:"default_root"`
+	CheckStatus bool
+}
+
+func (c Config) MustProxy() bool {
+	return c.OnlyProxy || c.OnlyLocal
+}
